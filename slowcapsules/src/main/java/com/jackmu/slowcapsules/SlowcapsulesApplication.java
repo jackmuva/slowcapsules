6650a21a99b6a94fc3424473981f7c6b121d78db
jackmuva <33766184+jackmuva@users.noreply.github.com>
2022-12-16 12:59:20 -0600

    Phew added back all files

---
+++ slowcapsules/src/main/java/com/jackmu/slowcapsules/SlowcapsulesApplication.java
@@ -0,0 +1,13 @@
+package com.jackmu.slowcapsules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowcapsulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowcapsulesApplication.class, args);
+	}
+
+}
---
+++ slowcapsules/src/test/java/com/jackmu/slowcapsules/SlowcapsulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackmu.slowcapsules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowcapsulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
