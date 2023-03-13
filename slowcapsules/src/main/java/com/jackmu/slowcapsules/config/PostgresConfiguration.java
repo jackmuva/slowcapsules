23d57fd42ad8beb7da14f244d20fd5ead22d336e
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-03-12 21:33:26 -0600

    Added in a config file for the datasource and disabled sunfire tests

---
+++ slowcapsules/src/main/java/com/jackmu/slowcapsules/config/PostgresConfiguration.java
@@ -0,0 +1,27 @@
+package com.jackmu.slowcapsules.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Configuration
+public class PostgresConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        final Map<String, String> env = System.getenv();
+        DataSourceBuilder builder = DataSourceBuilder.create();
+
+        String url = env.get("postgres_url");
+        String username = env.get("pg_username");
+        String password = env.get("pg_pw");
+
+        builder.driverClassName("org.postgresql.Driver")
+                .url(url)
+                .username(username)
+                .password(password);
+        return builder.build();
+    }
+}
