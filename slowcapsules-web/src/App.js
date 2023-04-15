c3290f891a9540ae37cae9b6c1ac17bb992ff2cd
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-04-15 10:33:29 -0500

    put in sample series data for the main page; replace with api call later

--- slowcapsules-web/src/App.js
+++ slowcapsules-web/src/App.js
@@ -1,11 +1,21 @@
-import logo from './main-page/logo.svg';
 import './App.css';
 import Header from "./main-page/header";
+import { useEffect, useState, useMemo } from "react";
 
 function App() {
+    const [allSeries, setAllSeries] = useState([]);
+    useEffect(() => {
+        const fetchSeries = async () => {
+            const rsp = await fetch("/sample-series.json");
+            const series = await rsp.json();
+            setAllSeries(series);
+        };
+        fetchSeries();
+    }, []);
+
   return (
     <div className="container">
-      <Header />
+      <Header subtitle = "Write and Subscribe to Email Series"/>
     </div>
   );
 }
--- slowcapsules-web/src/main-page/header.js
+++ slowcapsules-web/src/main-page/header.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-const Header = () =>(
+const Header = ({ subtitle }) =>(
     <header className = "row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="logo"/>
         </div>
         <div className = "col-md-7 mt-5 subtitle">
-            Write and Subscribe to Email Series
+            {subtitle}
         </div>
     </header>
 );
