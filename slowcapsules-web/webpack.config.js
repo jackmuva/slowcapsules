19fecaed338c8e90e6d316eb313c3ecaed56cc8d
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-04-29 11:01:24 -0500

    following a webpack tutorial by educative.io; doesn't work-next step is just to build a webpack project from scratch and then import my custom react scripts in

--- slowcapsules-web/webpack.config.js
+++ slowcapsules-web/webpack.config.js
@@ -1,37 +1,37 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, "/dist"), // the bundle output path
+        filename: "bundle.js", // the name of the bundle
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html", // to import index.html file inside index.js
+        }),
+    ],
+    devServer: {
+        port: 3030, // you can change the port
+    },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
+                test: /\.(js|jsx)$/, // .js and .jsx files
+                exclude: /node_modules/, // excluding the node_modules folder
+                use: {
+                    loader: "babel-loader",
+                },
             },
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                test: /\.(sa|sc|c)ss$/, // styles files
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
-                test: /\.svg$/,
-                use: "file-loader",
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+                loader: "url-loader",
+                options: { limit: false },
             },
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    output: {
-        path: path.resolve(__dirname, './public'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "./public")
-        }
     },
 };
