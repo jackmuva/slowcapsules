056177ee940f2e11f13c6fa82c370847419b0050
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-05-14 03:15:28 -0500

    starting with header styling

--- slowcapsules-web/src/main-page/header.js
+++ slowcapsules-web/src/main-page/header.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+import './header.css';
+
 const Header = ({ subtitle }) =>(
-    <header className = "row">
-        <div className="col-md-5">
-            <img src={logo} className="logo" alt="logo"/>
-        </div>
+    <header className = "Title">
+        <h1> Tide Library </h1>
+        <br></br>
         <div className = "col-md-7 mt-5 subtitle">
             {subtitle}
         </div>
