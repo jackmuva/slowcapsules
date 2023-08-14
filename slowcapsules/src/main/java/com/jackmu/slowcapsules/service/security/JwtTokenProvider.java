fe236f85b6e99bef3b55b9f9a1e7b8ab30dd8574
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-08-14 07:48:44 -0500

    Added in JWT code; still need to finish exceptions

---
+++ slowcapsules/src/main/java/com/jackmu/slowcapsules/service/security/JwtTokenProvider.java
@@ -0,0 +1,68 @@
+package com.jackmu.slowcapsules.service.security;
+
+import com.jackmu.slowcapsules.exception.SlowCapsuleAPIException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+    final Map<String, String> env = System.getenv();
+    private String jwtSecret = env.get("jwt-secret");
+    @Value("${app-jwt-expiration-milliseconds}")
+    private String jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String jwtToken = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return jwtToken;
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+    }
+
+    public String getUsername(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        String username = claims.getSubject();
+        return username;
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        }
+        catch (MalformedJwtException ex){
+            throw new SlowCapsuleAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT Token")
+        }
+    }
+}
