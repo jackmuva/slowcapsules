fce19bec9b5af5b4d1e4aa3a728b80b1f71a6ef0
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-09-23 11:13:03 -0500

    I sort of have the image repo setup, but need to create an S3 / local directory sort of setup instead of uploading images

--- slowcapsules/src/main/java/com/jackmu/slowcapsules/controller/ImageController.java
+++ slowcapsules/src/main/java/com/jackmu/slowcapsules/controller/ImageController.java
@@ -1,15 +1,17 @@
 package com.jackmu.slowcapsules.controller;
 
+import com.jackmu.slowcapsules.model.DownloadedImage;
 import com.jackmu.slowcapsules.model.Image;
+import com.jackmu.slowcapsules.model.UploadedImage;
 import com.jackmu.slowcapsules.service.ImageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -24,14 +26,20 @@ public class ImageController {
 
     @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/save", consumes = { "multipart/form-data" })
-    public void postImage(@ModelAttribute Image image){
-        imageService.saveImage(image);
+    public ResponseEntity<DownloadedImage> postImage(@ModelAttribute UploadedImage image) throws Exception{
+        Image rawImage = new Image();
+        rawImage.setImage(image.getImage().getBytes());
+        Image savedImage = imageService.saveImage(rawImage);
+        DownloadedImage downloadedImage = new DownloadedImage(1, Collections.singletonMap("url",
+                        "http://localhost:3000/api/image/get/" + savedImage.getImageId()));
+        return new ResponseEntity<>(downloadedImage, HttpStatus.OK);
     }
 
     //TODO: scheduled service that deletes unused images
 
     @GetMapping("/get/{imageId}")
-    public List<Image> getImageById(@PathVariable Long imageId){
-        return imageService.fetchImageByImageId(imageId);
+    public Image getImageById(@PathVariable Long imageId){
+        return null;
+//        return imageService.fetchImageByImageId(imageId).get(0).getImage();
     }
 }
---
+++ slowcapsules/src/main/java/com/jackmu/slowcapsules/model/UploadedImage.java
@@ -0,0 +1,14 @@
+package com.jackmu.slowcapsules.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadedImage {
+    private MultipartFile image;
+
+}
