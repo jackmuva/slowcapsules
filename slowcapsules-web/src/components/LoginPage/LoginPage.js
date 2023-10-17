2ec900056d31c5fd1ce309904a1a2215b6246374
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-10-17 00:05:04 -0500

    Styled the Signup page

--- slowcapsules-web/src/components/LoginPage/LoginPage.js
+++ slowcapsules-web/src/components/LoginPage/LoginPage.js
@@ -44,7 +44,7 @@ function LoginPage ({setWriter}) {
     else {
         return (
             <div class="flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl
-                md:flex-row md:space-y-0 md:m-12">
+                md:flex-row md:space-y-0 md:mx-52">
                 <div className="form" class="p-6 md:p-20">
                     <h2 class="font-serif test-4xl font-bold mb-2">Log In</h2>
                     <p class="mb-2 max-2-sm font-sans font-light text-gray-600">
@@ -55,7 +55,7 @@ function LoginPage ({setWriter}) {
                     <input type="password" id="password" class="h-1 w-full p-6 mb-2 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
                            placeholder="Password"
                            onChange={(e) => handleInputChange(e)}/>
-                    {errorMessage && <div className="error"> {errorMessage} </div>}
+                    {errorMessage && <div class="text-red-700 my-2"> {errorMessage} </div>}
                     <div className="footer">
                         <button class="w-full md:w-auto h-1 flex justify-center items-center p-6 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
                                 onClick={() => handleSubmit()} type="submit">Login</button>
--- slowcapsules-web/src/components/SignUpPage/index.js
+++ slowcapsules-web/src/components/SignUpPage/index.js
@@ -56,28 +56,29 @@ function SignUpPage () {
     }
     else{
         return(
-            <div className="form">
-                <div className="form-body">
-                    <div className="username">
-                        <label className="form__label" htmlFor="username">Username </label>
-                        <input className="form__input" type="text" id="username" placeholder="Username" onChange = {(e) => handleInputChange(e)}/>
-                    </div>
-                    <div className="email">
-                        <label className="form__label" htmlFor="email">Email </label>
-                        <input type="email" id="email" className="form__input" placeholder="Email" onChange = {(e) => handleInputChange(e)}/>
-                    </div>
-                    <div className="password">
-                        <label className="form__label" htmlFor="password">Password </label>
-                        <input className="form__input" type="password" id="password" placeholder="Password" onChange = {(e) => handleInputChange(e)}/>
-                    </div>
-                    <div className="confirm-password">
-                        <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
-                        <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password" onChange = {(e) => handleInputChange(e)}/>
+            <div className="flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl
+                md:flex-row md:space-y-0 md:mx-52">
+                <div className="form" className="p-6 md:p-20">
+                    <h2 className="font-serif test-4xl font-bold mb-2">Sign Up</h2>
+                    <p className="mb-2 max-2-sm font-sans font-light text-gray-600">
+                        Sign up to publish email series and share with other readers
+                    </p>
+                    <input type="text" id="username" class="h-1 w-full p-6 mb-2 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
+                           placeholder="Username" onChange = {(e) => handleInputChange(e)}/>
+                    <input type="email" id="email" class="h-1 w-full p-6 mb-2 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
+                           placeholder="Email" onChange = {(e) => handleInputChange(e)}/>
+                    <input type="password" id="password" class="h-1 w-full p-6 mb-2 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
+                           placeholder="Password" onChange = {(e) => handleInputChange(e)}/>
+                    <input type="password" id="confirmPassword" class="h-1 w-full p-6 mb-2 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
+                           placeholder="Confirm Password" onChange = {(e) => handleInputChange(e)}/>
+                    {errorMessage && <div class="text-red-700 my-2"> {errorMessage} </div>}
+                    <div className="footer">
+                        <button class="w-full md:w-auto h-1 flex justify-center items-center p-6 space-x-4 font-sans font-bold text-white rounded-md shadow-lg px-9 bg-cyan-700 shadow-cyan-100 hover:bg-opacity-90 shadow-sm hover:shadow-lg border transition hover:-translate-y-0.5 duration-150"
+                                onClick={() => handleSubmit()} type="submit">Register</button>
                     </div>
                 </div>
-                {errorMessage && <div className="error"> {errorMessage} </div>}
-                <div className="footer">
-                    <button onClick={() => handleSubmit()} type="submit" className="btn">Register</button>
+                <div>
+                    <img src="images/vanc-lake.png" className="h-full hidden md:block rounded-r-2xl" alt=""/>
                 </div>
             </div>);
     }
