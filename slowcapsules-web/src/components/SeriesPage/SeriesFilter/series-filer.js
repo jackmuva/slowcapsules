7651d9bab8f18c526409f0cb204b72c4cf1b62d0
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-10-18 19:20:49 -0500

    styled the search bar

--- slowcapsules-web/src/components/SeriesPage/SeriesFilter/series-filer.js
+++ slowcapsules-web/src/components/SeriesPage/SeriesFilter/series-filer.js
@@ -17,19 +17,21 @@ const SeriesFilter = ({ posts, setSearchResults }) => {
     }
 
     return (
-        <header>
-            <form className="search" onSubmit={handleSubmit}>
+        <div className="mb-1">
+            <div className="relative mb-0 flex w-1/4 flex-wrap items-stretch">
                 <input
-                    className="search__input"
-                    type="text"
+                    type="search"
+                    className="relative m-4 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                    placeholder="Search"
+                    aria-label="Search"
                     id="search"
                     onChange={handleSearchChange}
                 />
-                <button className="search__button">
+                <button onSubmit={handleSubmit}>
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </button>
-            </form>
-        </header>
+            </div>
+        </div>
     )
 }
 
