1259c4b81c4a6291394a057e2f8d614c1d24672f
jackmuva <33766184+jackmuva@users.noreply.github.com>
2023-11-20 15:53:16 -0600

    Modified depoly script and removed json file

--- slowcapsules-web/deploy.sh
+++ slowcapsules-web/deploy.sh
@@ -8,6 +8,9 @@ echo "Build Steps:"
 echo "building image..."
 docker build -t 514832027284.dkr.ecr.us-east-1.amazonaws.com/slowcapsules:1 -t 514832027284.dkr.ecr.us-east-1.amazonaws.com/slowcapsules:latest .
 
+echo "updating AWS ECS service..."
+#aws ecs update-service --cluster <cluster-name> --service <service-name> --force-new-deployment
+
 echo "Post-Build steps:"
 echo "pushing image to AWS ECR"
 docker push --all-tags 514832027284.dkr.ecr.us-east-1.amazonaws.com/slowcapsules
